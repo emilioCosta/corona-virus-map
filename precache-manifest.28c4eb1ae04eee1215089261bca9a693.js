749069fb60508b404188fcfcc755cc7ae7adee21
Emilio <emilio.costa@dextra-sw.com>
2020-03-12 20:59:03 -0300

    Updates

---
+++ precache-manifest.28c4eb1ae04eee1215089261bca9a693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703620b1af0beb1bed2e433d090a07a0",
+    "url": "/corona-virus-map/index.html"
+  },
+  {
+    "revision": "509fea201261378c9f51",
+    "url": "/corona-virus-map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3296dae2795b93a5587",
+    "url": "/corona-virus-map/static/js/2.65c45d3d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/corona-virus-map/static/js/2.65c45d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509fea201261378c9f51",
+    "url": "/corona-virus-map/static/js/main.fe085b23.chunk.js"
+  },
+  {
+    "revision": "9d6bed532db53786cd2b",
+    "url": "/corona-virus-map/static/js/runtime-main.0dd6125f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona-virus-map/static/media/logo.5d5d9eef.svg"
+  }
+]);
